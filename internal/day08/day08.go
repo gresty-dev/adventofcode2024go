82709d741cda0ac73dffb2e9e2a9644894781b16
Chris Gresty <chris@gresty.dev>
2024-12-08 17:26:10 +0000

    use dot-imports for brevity

--- internal/day08/day08.go
+++ internal/day08/day08.go
@@ -5,21 +5,21 @@ import (
 	"image"
 	"io"
 
-	aoc "go.gresty.dev/aoc2024/internal/lib"
+	. "go.gresty.dev/aoc2024/internal/lib"
 )
 
-type pointSet map[image.Point]aoc.Void
+type pointSet map[image.Point]Void
 type pointList []image.Point
 
 type antinodeFinder func(image.Point, image.Point, image.Rectangle) pointSet
 
-func Execute(input io.Reader) (aoc.Result, aoc.Result) {
+func Execute(input io.Reader) (Result, Result) {
 	rect, antennae := readInput(input)
-	result1 := aoc.NewResult(func() any {
+	result1 := NewResult(func() any {
 		return len(findAntinodes(rect, antennae, antinodesForPair))
 	})
 
-	result2 := aoc.NewResult(func() any {
+	result2 := NewResult(func() any {
 		return len(findAntinodes(rect, antennae, allAntinodesForPair))
 	})
 
@@ -29,7 +29,7 @@ func Execute(input io.Reader) (aoc.Result, aoc.Result) {
 func findAntinodes(rect image.Rectangle, antennae map[byte]pointList, finder antinodeFinder) pointSet {
 	antinodes := pointSet{}
 	for _, v := range antennae {
-		aoc.CombineSets(antinodes, antinodesForFrequency(rect, v, finder))
+		CombineSets(antinodes, antinodesForFrequency(rect, v, finder))
 	}
 	return antinodes
 }
@@ -37,8 +37,8 @@ func findAntinodes(rect image.Rectangle, antennae map[byte]pointList, finder ant
 func antinodesForFrequency(rect image.Rectangle, nodes pointList, finder antinodeFinder) pointSet {
 	antinodes := pointSet{}
 
-	aoc.ForEachPair(nodes, func(a, b image.Point) {
-		aoc.CombineSets(antinodes, finder(a, b, rect))
+	ForEachPair(nodes, func(a, b image.Point) {
+		CombineSets(antinodes, finder(a, b, rect))
 	})
 	return antinodes
 }
@@ -48,26 +48,26 @@ func antinodesForPair(a, b image.Point, rect image.Rectangle) pointSet {
 	antinodes := pointSet{}
 	an1 := b.Add(offset)
 	if an1.In(rect) {
-		antinodes[an1] = aoc.Empty
+		antinodes[an1] = Empty
 	}
 	an2 := a.Sub(offset)
 	if an2.In(rect) {
-		antinodes[an2] = aoc.Empty
+		antinodes[an2] = Empty
 	}
 	return antinodes
 }
 
 func allAntinodesForPair(a, b image.Point, rect image.Rectangle) pointSet {
 	offset := b.Sub(a)
-	gcd := aoc.Gcd(offset.X, offset.Y)
+	gcd := Gcd(offset.X, offset.Y)
 	incr := offset.Div(gcd)
 
 	antinodes := pointSet{}
 	for an := a; an.In(rect); an = an.Add(incr) {
-		antinodes[an] = aoc.Empty
+		antinodes[an] = Empty
 	}
 	for an := a; an.In(rect); an = an.Sub(incr) {
-		antinodes[an] = aoc.Empty
+		antinodes[an] = Empty
 	}
 
 	return antinodes
